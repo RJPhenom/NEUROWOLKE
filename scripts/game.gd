9a328c5049b2926477a2cd9f01ccedae3368cfff
RJ <103343995+RJPhenom@users.noreply.github.com>
2025-05-09 17:58:47 -0400

    Convo pass, game pass fail logic added

--- scripts/game.gd
+++ scripts/game.gd
@@ -9,6 +9,9 @@ var dialogue_theme = load("res://styles/tros_theme.tres")
 
 # Subject controls
 var subject_details: Label
+var subject_tracker: int = 1
+
+var win: bool = false
 
 # Begin play
 func _ready():
@@ -19,13 +22,8 @@ func _ready():
 	
 	subject_details = get_node("DialogueMargin/DialogueVbox/SubjectHbox/SubjectDetails")
 	
-	# Sets the first subject details
-	var subject = get_subject(0)
-	set_subject_details(subject)
-	
-	# Opens the first step of dialogue options
-	var opening_options = get_dialogue_options(001)
-	write_to_dialogue_options(opening_options)
+	# Initializes the first subject conversation
+	init_new_subject(subject_tracker)
 
 # Fetchs dialogue by switch ID
 func get_dialogue_response(id: int) -> Array:
@@ -35,7 +33,7 @@ func get_dialogue_response(id: int) -> Array:
 		001002:
 			return ["Response two.", 001]
 		001003:
-			return ["Response three.", 001]
+			return ["END_WIN_HUMAN", 000]
 		_:
 			return ["Unknown option: The void stares back.", 000]
 
@@ -53,6 +51,41 @@ func get_dialogue_options(id: int) -> Array:
 
 # Wrties to the history label/scrollbox
 func write_to_dialogue_history(text: String):
+	var win_states = [
+		"//SUBJECT:   END_WIN_HUMAN",
+		"//SUBJECT:   END_WIN_KUNST",
+	]
+	
+	var lose_states = [
+		"//SUBJECT:   END_LOSE_HUMAN",
+		"//SUBJECT:   END_LOSE_KUNST"
+	]
+	
+	if text in win_states:
+		# Debug
+		print("Conversation End state reached. Player is currently winning.")
+		
+		win = true
+		clear_dialogue_options()
+		
+		# TODO: Add convo end anim
+		subject_tracker += 1	
+		init_new_subject(subject_tracker)
+		
+		return
+	
+	elif text in lose_states:
+		# Debug
+		print("Conversation End state reached. Player has lost.")
+		
+		win = false
+		clear_dialogue_options()
+		
+		# TODO: Add convo end anim
+		init_new_subject(0)
+		
+		return
+	
 	var history = dialogue_history.text
 	history += "\n\n\n" + text
 	
@@ -61,11 +94,23 @@ func write_to_dialogue_history(text: String):
 	await get_tree().create_timer(0.2).timeout
 	dialogue_scrollbox.scroll_vertical = dialogue_scrollbox.get_v_scroll_bar().max_value
 
-# Writes options to the vbox as buttons for dialogue interaction
-func write_to_dialogue_options(options: Array):
+# Clears the dialogue options vbox of children
+func clear_dialogue_options():
 	for child in dialogue_options.get_children():
 		child.queue_free()
+
+# Writes options to the vbox as buttons for dialogue interaction
+func write_to_dialogue_options(options: Array):
+	if options.is_empty():
+		# Debug
+		print("Game End state reached.")
+		
+		if win: get_tree().call_deferred("change_scene_to_file", "res://scenes/win_screen.tscn")
+		else: get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
+		
+		return
 	
+	clear_dialogue_options()
 	for i in options.size():
 		var option = options[i]
 		var option_button = Button.new()
@@ -94,7 +139,24 @@ func set_subject_details(subject: Array):
 # Fetches and sets subject details
 func get_subject(subject_id: int) -> Array:
 	match subject_id:
-		0:
-			return ["CLIVE ANDERSON", "ACADIAN COLONIES", "DATA THEFT"]
+		1:
+			return ["CLIVE ANDERSON", "ACADIAN COLONIES", "DATA THEFT", 001, 
+				"//:   Your first subject is brought to you. He sits in a chair and a Shinka-seishi Cyberdeck is placed on his head."
+				+ "It’s purpose is to help monitor the subjects responses."
+				+ "\n\n\n"
+				+ "It’s second function is to allow you to eliminate."]
 		_:
-			return ["[SUBJECT]", "[ORIGIN]", "[CRIME]"]
+			return ["[SUBJECT]", "[ORIGIN]", "[CRIME]", 000, ""]
+
+# Initializes a new subject and conversation
+func init_new_subject(subject_id: int):
+	# Fetch subject
+	var subject = get_subject(subject_id)
+	
+	# Write details and opening
+	set_subject_details(subject)
+	write_to_dialogue_history(subject[4])
+	
+	# Init dialogue options
+	var opening_options = get_dialogue_options(subject[3])
+	if !opening_options.is_empty(): write_to_dialogue_options(opening_options)
